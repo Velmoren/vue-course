9234fe1ec44efaa31476892c9d1b21a7c93453ef
st-it-01 <ilyasov_as@creacept.ru>
2020-10-26 13:20:09 +0300

    add first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>{{msg}}</h2>
+
+    <app-car></app-car>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
